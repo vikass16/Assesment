7c26ce75a1198ae9c6dff4c0b3898e9a79bf174f
vikass16 <vk7199234@gmail.com>
2026-08-14 23:25:37 +0530

    Basic Problem of Recursion

--- recursion/Print1To5.java
+++ recursion/Print1To5.java
@@ -4,6 +4,8 @@ public class Print1To5 {
     public static void main(String[] args) {
         // write a function that prints 1 to 5
         print(1);
+        print1(5);
+        printReverse(5);
     }
 
     static void print(int n){
@@ -13,5 +15,24 @@ public class Print1To5 {
         }
         System.out.println(n);
         print(n+1);
+    }
+
+    static void print1(int n){
+        if(n == 1){
+            System.out.println(n);
+            return;
+        }
+        print1(n-1);
+        System.out.println(n);
+
+    }
+
+    static void printReverse(int n){
+        if(n == 1){
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        printReverse(n-1);
     }
 }
